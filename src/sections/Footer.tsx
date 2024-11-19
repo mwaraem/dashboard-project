c58f598a1ba59c9ba17e43abf9004ede6d3cd3c2
mwaraem <mwaraem@gmail.com>
2024-11-19 17:47:19 +0300

    First Commit

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,41 @@
+import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg'
+
+const footerLinks = [
+  {
+    title: 'Linkedin',
+    href: '#',
+  },
+  {
+    title: 'Twitter',
+    href: '#',
+  },
+  {
+    title: 'Instagram',
+    href: '#',
+  },
+  {
+    title: 'Github',
+    href: '#',
+  },
+]
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div>
+          <div>&copy; 2024. All rights reserved</div>
+          <nav>
+            {footerLinks.map(link => (
+              <a href="#" key={link.title}>
+                <span>{link.title}</span>
+                <ArrowUpRightIcon />
+              </a>
+            ))}
+          </nav>
+          <div></div>
+        </div>
+      </div>
+    </footer>
+  )
 };
