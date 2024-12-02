5e2c36f84011e98b6344521dfe154029747834b4
mwaraem <mwaraem@gmail.com>
2024-12-03 01:14:10 +0300

    Fixed Issue

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -70,7 +70,7 @@ export const HeroSection = () => {
           <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">Building Exceptional User Experiences</h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">
             I specialize in transforming designs into functional, high-performing
-            web applications. Let's discuss your next project.
+            web applications. Let&apos;s discuss your next project.
           </p>
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
@@ -80,7 +80,7 @@ export const HeroSection = () => {
           </button>
           <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
             <span>👋</span>
-            <span className="font-semibold">Let's Connect</span>
+            <span className="font-semibold">Let&apos;s Connect</span>
           </button>
         </div>
       </div>
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -4,7 +4,6 @@ import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import Image from "next/image";
 import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg"
-import grainImage from "@/assets/images/grain.jpg";
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
 
@@ -47,6 +46,7 @@ const portfolioProjects = [
   },
 ];
 
+
 export const ProjectsSection = () => {
   return (
     <section className="pb-16 lg:py-24">
@@ -55,8 +55,8 @@ export const ProjectsSection = () => {
 
         <div className="mt-10 md:mt-20 flex flex-col gap-20">
           {portfolioProjects.map((project, projectIndex) => (
-            <Card key={project.title} className=" px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky" style={{
-              top: `calc(64px + ${projectIndex * 40}px)`
+            <Card key={project.title} className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky" style={{
+              top: `calc(64px + ${projectIndex * 40}px)`,
             }}>
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
@@ -69,10 +69,10 @@ export const ProjectsSection = () => {
                   <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>
                   <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
                   <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-                    {project.results.map(result => (
-                      <li className="flex gap-2 text-sm md:text-base text-white/50">
+                    {project.results.map(results => (
+                      <li key={projectIndex} className="flex gap-2 text-sm md:text-base text-white/50">
                         <CheckCircleIcon className="size-5 md:size-6" />
-                        <span>{result.title}</span>
+                        <span>{results.title}</span>
                       </li>
                     ))}
                   </ul>
